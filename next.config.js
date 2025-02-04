ec3f07317d90eacbb7a28916666feae6493765bc
Braden <braden@example.com>
2025-02-04 14:49:02 +0800

    chore: add webpack alias for @/hooks to resolve use-performance module error

--- next.config.js
+++ next.config.js
@@ -12,3 +12,19 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+const path = require('path');
+
+// Add alias for '@/hooks'
+if (!module.exports.webpack) {
+  module.exports.webpack = (config) => {
+    config.resolve.alias['@/hooks'] = path.join(__dirname, 'hooks');
+    return config;
+  };
+} else {
+  const originalWebpack = module.exports.webpack;
+  module.exports.webpack = (config, options) => {
+    config.resolve.alias['@/hooks'] = path.join(__dirname, 'hooks');
+    return originalWebpack(config, options);
+  };
+}
