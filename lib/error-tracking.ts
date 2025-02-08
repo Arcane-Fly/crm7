345fecfa70219a304f236d16482e2281a2fae6d7
GaryOcean <81794144+GaryOcean428@users.noreply.github.com>
2025-02-08 16:46:58 +0800

    Update import statement in `lib/error-tracking.ts` and add setting to `.npmrc`
    * Import `toast` from `@/components/ui/use-toast` in `lib/error-tracking.ts`
    * Import `punycode` from `punycode2` in `lib/error-tracking.ts`
    * Add `global-bin-dir` setting in `.npmrc` to specify the global bin directory

--- lib/error-tracking.ts
+++ lib/error-tracking.ts
@@ -1,4 +1,5 @@
-import { toast } from '@/hooks/use-toast';
+import { toast } from '@/components/ui/use-toast';
+import punycode from 'punycode2';
 
 interface ErrorContext {
   componentName?: string;
